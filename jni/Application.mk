81c391a1672d98bc2f707bb9b05473f0a4170b6c
Charley Robinson <charley@tokbox.com>
2013-07-13 08:47:35 -0700

    Build all libs plz

--- jni/Application.mk
+++ jni/Application.mk
@@ -1,5 +1,5 @@
 NDK_TOOLCHAIN_VERSION=4.4.3
 APP_PROJECT_PATH := $(shell pwd)
 APP_BUILD_SCRIPT := $(APP_PROJECT_PATH)/Android.mk
-APP_MODULES := libssl_static libcrypto_static
+APP_MODULES := libssl_static libcrypto_static libssl libcrypto
 APP_ABI := all
